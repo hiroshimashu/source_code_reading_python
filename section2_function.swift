5c872e9527dbc308835cd4238dfa9bad658e9315
hiroshimashu <hiroshimashu@gmail.com>
2019-03-27 20:59:36 +0900

    finish implement

---
+++ section2_function.swift
@@ -0,0 +1,12 @@
+//
+//  section2_function.swift
+//  
+//
+//  Created by Hiroshi Imamasu on 2019/03/24.
+//
+
+import Foundation
+
+func say(_s:String, times: Int, loudly:Bool) {
+    loudly = true
+}
